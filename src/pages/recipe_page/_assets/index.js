a25c1a23ebf06234c7869d80c3538cbe628e0944
adiazt01 <adiaztovar2004@gmail.com>
2024-04-08 10:28:49 -0400

    Update recipe page assets and layout configuration

---
+++ src/pages/recipe_page/_assets/index.js
@@ -0,0 +1,2 @@
+export {default as ImageOmellete } from "./image_omelette.jpeg"
+export {default as FaviconRecipePage } from "./favicon_32x32.png"
--- src/pages/recipe_page/assets/index.js
+++
@@ -1,2 +0,0 @@
-export {default as ImageOmellete } from "./image-omelette.jpeg"
-export {default as FaviconRecipePage } from "./favicon-32x32.png"
